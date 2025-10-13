36e645f4bd08f155e1eca8e231ad98d8b7a77a37
JuanAntonioSolis <jsolcor457@g.educaand.es>
2025-10-13 19:01:53 +0200

    lunes puente

--- UD2/Examen24/ej4/4.js
+++ UD2/Examen24/ej4/4.js
@@ -1,13 +1,14 @@
-function piramide(char,num){
-    for(let i=num;i>1;i++){
-            for(let j=num;j>num;j++){
-                document.write(char);
-            }
-            document.write("<br>");
+function piramide(char, num) {
+    for (let i = num; i > 0; i--) {
+        
+        for (let j = 0; j < i; j++) {
+            document.write(char);
         }
+        document.write("<br>");
+    }
 }
 
 let caracter = prompt("Elige un carácter:");
 let numero = parseInt(prompt("Elige un número:"));
 
-piramide(caracter,numero);
+piramide(caracter, numero);
