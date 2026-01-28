84d8e0a7646c968e315488a8186a961be5f8f1b0
JuanAntonioSolis <jsolcor457@g.educaand.es>
2026-01-28 12:21:04 +0100

    28JanMorning

--- UD6/EjemploExamen/Ejercicio3/script.js
+++ UD6/EjemploExamen/Ejercicio3/script.js
@@ -1,7 +1,47 @@
-document.addEventListener("DOMContentLoaded",() => {
+document.addEventListener("DOMContentLoaded", () => {
+
+    const parrafos = document.querySelectorAll("p");
+
+    parrafos.forEach(p => {
+        p.addEventListener("click",cambiarTexto,false);
+        p.addEventListener("mouseover",resaltarTexto,false);
+        p.addEventListener("mouseout",resaltarTexto,false);
+        
+    });
+
+    document.querySelector("#contar").addEventListener("click",contarParrafos);
 
 });
 
-function manejador(e){
-    
+function cambiarTexto(e){
+    if(e.target.closest("p")){
+        const elemento = e.target.closest("p");
+
+        elemento.textContent = prompt("Escribe un nuevo contenido del párrafo:");
+    }
 }
+
+function resaltarTexto(e){
+
+    const elemento = e.target.closest("p");
+
+    if(e.target.closest("p")){
+        if (e.type == "mouseover"){
+            elemento.style.backgroundColor = "lightgrey";
+            elemento.style.fontSize = "1.6em";
+        } else if(e.type == "mouseout"){
+            elemento.style.backgroundColor = "";
+            elemento.style.fontSize = "1.5em";
+        }
+    }
+}
+
+function contarParrafos(){
+    const cuenta = document.querySelectorAll("p").length;
+
+    alert(`Hay ${cuenta} párrafos en el documento`);
+}
+
+
+
+
